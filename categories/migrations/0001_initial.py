5fdc7814fad6c93c2632e9d84f7555a90de14c85
Deepika Venkatesan <64186626+Dipika2020@users.noreply.github.com>
2024-06-22 14:55:08 -0400

    Create 0001_initial.py

---
+++ categories/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=50)),
+                ('description', models.TextField()),
+            ],
+        ),
+    ]
