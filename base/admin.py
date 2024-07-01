50c7aed113f87ace4a1ad7f10ed3a0154bb2fa5d
Deepika Venkatesan <64186626+Dipika2020@users.noreply.github.com>
2024-06-30 23:33:14 -0400

    updated ratings and categories models and made migrations

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# base/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Rating
+
+admin.site.register(Category)
+admin.site.register(Rating)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,36 @@
+# base/models.py
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
+from cars.models import Cars
 
-# Create your models here.
+
+# base/models.py
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Cars, on_delete=models.CASCADE)  # ForeignKey to Cars model
+    rating = models.DecimalField(max_digits=3, decimal_places=2)  # Allow for decimal ratings
+    comment = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.car.companyName} {self.car.name} Rating"
+
+    class Meta:
+        ordering = ['-date_created']  # Order ratings by date created descending
