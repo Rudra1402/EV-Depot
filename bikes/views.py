dcc5437590fff60e917b00f0922d7b0cfbe78d28
Rudra Patel <rudra.190410116095@gmail.com>
2024-07-01 22:37:28 -0400

    Added dummy data to work on UI

--- bikes/views.py
+++ bikes/views.py
@@ -4,7 +4,29 @@ from django.http import HttpResponse
 from bikes.models import Bikes
 # Create your views here.
 def index(request):
-    bikes = Bikes.objects.all()
+    # bikes = Bikes.objects.all()
+    bikes = [
+        {
+            "name":"Speedster 500",
+            "companyName":"FastBikes Inc.",
+            "modelType":"road",
+            "manufacturingYear":2023,
+            "price":1299.99,
+            "description":"A high-performance road bike designed for speed and efficiency.",
+            "image":"https://media.istockphoto.com/id/1415317051/photo/black-electric-bike-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=KLlFOJEFWPpSDe9TWB2kqGKS2GuvNp_mhe7y53wnIfA=",
+            "isNew":True
+        },
+        {
+            "name":"Speedster 500",
+            "companyName":"FastBikes Inc.",
+            "modelType":"road",
+            "manufacturingYear":2023,
+            "price":1299.99,
+            "description":"A high-performance road bike designed for speed and efficiency.",
+            "image":"https://media.istockphoto.com/id/1415317051/photo/black-electric-bike-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=KLlFOJEFWPpSDe9TWB2kqGKS2GuvNp_mhe7y53wnIfA=",
+            "isNew":True
+        }
+    ]
     context = {
         'bikes': bikes
     }
