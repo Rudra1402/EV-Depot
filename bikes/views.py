31aac258b4a2c6e765a3f54916a7a0e468be8635
Rudra Patel <rudra.190410116095@gmail.com>
2024-07-10 19:41:21 -0400

    delete bike with icons

--- bikes/views.py
+++ bikes/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, Http404
+from django.views.decorators.http import require_http_methods
 
 from bikes.models import Bikes
 from .forms import BikeForm
@@ -25,3 +26,13 @@ def index(request):
 def bikeById(request, id):
     bike = get_object_or_404(Bikes, id=id)
     return render(request, 'bike.html', {'bike': bike})
+
+def delete_bike(request, id):
+    if request.method == 'DELETE':
+        print(f"Received DELETE request for bike ID {id}")  # Debug statement
+        bike = get_object_or_404(Bikes, pk=id)
+        bike.delete()
+        return JsonResponse({"message": "Bike deleted successfully."}, status=200)
+    else:
+        print(f"Received {request.method} request, only DELETE is allowed")  # Debug statement
+        raise Http404("Only DELETE method is allowed")
