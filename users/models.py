09b8996a653826cf9c086cbd52ce20db56bd50f7
chintan0513 <chintantripathi1310@gmail.com>
2024-07-17 01:04:38 -0400

    validation added to registration

--- users/models.py
+++ users/models.py
@@ -1,60 +1,60 @@
 # from django.db import models
-# from django.contrib.auth.models import User
+# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 #
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, firstname, lastname, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError("The Email field must be set")
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, firstname=firstname, lastname=lastname, **extra_fields)
+#         user.set_password(password)  # This hashes the password
+#         user.save(using=self._db)
+#         return user
 #
-# class CustomUser(models.Model):
+#     def create_superuser(self, email, firstname, lastname, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+#
+#         return self.create_user(email, firstname, lastname, password, **extra_fields)
+#
+# class CustomUser(AbstractBaseUser):
 #     GENDER_CHOICES = (
 #         ('M', 'Male'),
 #         ('F', 'Female'),
 #         ('O', 'Other'),
 #     )
 #
+#     email = models.EmailField(unique=True)
 #     firstname = models.CharField(max_length=30)
 #     lastname = models.CharField(max_length=30)
-#     email = models.EmailField(max_length=30, default='abc@gmail.com')
-#     password = models.CharField(max_length=30, default='abc')
 #     mobile = models.CharField(max_length=10)
 #     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 #
+#     objects = CustomUserManager()
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['firstname', 'lastname']
+#
 #     def __str__(self):
-#         return self.firstname
+#         return self.email
+
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, firstname, lastname, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, firstname=firstname, lastname=lastname, **extra_fields)
-        user.set_password(password)  # This will hash the password
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, firstname, lastname, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, firstname, lastname, password, **extra_fields)
-
-class CustomUser(AbstractBaseUser):
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
-    )
-
-    email = models.EmailField(unique=True)
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    mobile = models.CharField(max_length=10)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['firstname', 'lastname']
+class Buyer(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE, max_length=80, unique=True, blank=True)
+    firstname = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(max_length=80, unique=True)
+    mobile = models.CharField(max_length=11, null=False)
+    address = models.CharField(max_length=100, null=False)
+    city = models.CharField(max_length=100, null=False)
 
     def __str__(self):
-        return self.email
+        return f"{self.username} - {self.email}"
+
+    def _str_(self):
+        return str(self.firstname)
+
--- users/views.py
+++ users/views.py
@@ -1,45 +1,86 @@
-from django.shortcuts import render, redirect
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from django.contrib.auth import authenticate, login
+# from .forms import UserRegisterForm, UserLoginForm
+# from .models import CustomUser
+#
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             CustomUser.objects.create_user(
+#                 email=form.cleaned_data['email'],
+#                 firstname=form.cleaned_data['firstname'],
+#                 lastname=form.cleaned_data['lastname'],
+#                 password=form.cleaned_data['password'],
+#                 mobile=form.cleaned_data['mobile'],
+#                 gender=form.cleaned_data['gender']
+#             )
+#             messages.success(request, "Account created successfully!")
+#             return redirect('base:home')
+#         else:
+#             messages.warning(request, "Please correct the errors below.")
+#     else:
+#         form = UserRegisterForm()
+#
+#     return render(request, 'register.html', {'form': form})
+#
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=email, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 messages.success(request, "Logged in successfully!")
+#                 return redirect('base:home')
+#             else:
+#                 messages.error(request, "Invalid email or password")
+#     else:
+#         form = UserLoginForm()
+#
+#     return render(request, 'login.html', {'form': form})
+
+
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from .forms import UserRegisterForm, UserLoginForm
-from .models import CustomUser
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from .models import Buyer
 
-def register(request):
+def Register(request):
+    if request.method == 'GET':
+        return render(request, "register.html")
+
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            CustomUser.objects.create_user(
-                email=form.cleaned_data['email'],
-                firstname=form.cleaned_data['firstname'],
-                lastname=form.cleaned_data['lastname'],
-                password=form.cleaned_data['password'],
-                mobile=form.cleaned_data['mobile'],
-                gender=form.cleaned_data['gender']
-            )
-            messages.success(request, "Account created successfully!")
-            return redirect('users:register')
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        email = request.POST['email']
+        password = request.POST['password']
+        mobile = request.POST['mobile']
+        address = request.POST['address']
+        city = request.POST['city']
+
+        if len(mobile) != 10 or not mobile.isdigit():
+            messages.warning(request, "The phone number provided is not 10 digits!")
+        elif mobile.startswith('0'):
+            messages.warning(request, "The phone number provided is not valid!")
         else:
-            messages.warning(request, "Please correct the errors below.")
-    else:
-        form = UserRegisterForm()
-
-    return render(request, 'register.html', {'form': form})
-
-
-def login_view(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, "Logged in successfully!")
-                return redirect('bikes:homepage')  # Redirect to some view after login
-            else:
-                messages.error(request, "Invalid email or password")
-    else:
-        form = UserLoginForm()
-
-    return render(request, 'login.html', {'form': form})
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                Buyer.objects.create(username=user, firstname=firstname, email=email, mobile=mobile, address=address, city=city)
+                messages.success(request, "Account created successfully!")
+                return redirect('base:home')
+            except IntegrityError:
+                messages.warning(request, "Account already exists!")
+                return redirect('users:register')
+        return render(request, "register.html")
