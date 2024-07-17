6997c186ae9d577a61cb047bd44d492ef361a3ed
chintan0513 <chintantripathi1310@gmail.com>
2024-07-16 22:26:48 -0400

    login view

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,15 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class UserRegisterForm(forms.ModelForm):
     mobile = forms.CharField(max_length=10)
-    # gender = forms.ChoiceField(choices=CustomUser.GENDER_CHOICES)
+    gender = forms.ChoiceField(choices=CustomUser.GENDER_CHOICES)
 
     class Meta:
         model = CustomUser
-        fields = ['firstname', 'lastname', 'email', 'password', 'mobile']
-        labels = {'firstname': 'First Name', 'lastname': 'Last Name', 'email': 'Email', 'password': 'Password', 'mobile': 'Mobile'}
+        fields = ['firstname', 'lastname', 'email', 'password', 'mobile', 'gender']
+        labels = {'firstname': 'First Name', 'lastname': 'Last Name', 'email': 'Email', 'password': 'Password', 'mobile': 'Mobile', 'gender': 'Gender'}
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(max_length=30)
+    password = forms.CharField(widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -1,27 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm, UserLoginForm
 from .models import CustomUser
-
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            custom_user = CustomUser(
+            CustomUser.objects.create_user(
+                email=form.cleaned_data['email'],
                 firstname=form.cleaned_data['firstname'],
                 lastname=form.cleaned_data['lastname'],
-                email=form.cleaned_data['email'],
-                password=form.cleaned_data['password'],  # Consider hashing passwords
+                password=form.cleaned_data['password'],
                 mobile=form.cleaned_data['mobile'],
                 gender=form.cleaned_data['gender']
             )
-            custom_user.save()
             messages.success(request, "Account created successfully!")
-            return redirect('users:login')
+            return redirect('users:register')
         else:
             messages.warning(request, "Please correct the errors below.")
     else:
         form = UserRegisterForm()
 
     return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Logged in successfully!")
+                return redirect('bikes:homepage')  # Redirect to some view after login
+            else:
+                messages.error(request, "Invalid email or password")
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'login.html', {'form': form})
