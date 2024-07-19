527e74654f091bcb0e8844ce7f7e640dd148399a
chintan0513 <chintantripathi1310@gmail.com>
2024-07-19 17:44:21 -0400

    session-cookie added

--- users/views.py
+++ users/views.py
@@ -1,49 +1,3 @@
-# from django.shortcuts import render, redirect
-# from django.contrib import messages
-# from django.contrib.auth import authenticate, login
-# from .forms import UserRegisterForm, UserLoginForm
-# from .models import CustomUser
-#
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(request.POST)
-#         if form.is_valid():
-#             CustomUser.objects.create_user(
-#                 email=form.cleaned_data['email'],
-#                 firstname=form.cleaned_data['firstname'],
-#                 lastname=form.cleaned_data['lastname'],
-#                 password=form.cleaned_data['password'],
-#                 mobile=form.cleaned_data['mobile'],
-#                 gender=form.cleaned_data['gender']
-#             )
-#             messages.success(request, "Account created successfully!")
-#             return redirect('base:home')
-#         else:
-#             messages.warning(request, "Please correct the errors below.")
-#     else:
-#         form = UserRegisterForm()
-#
-#     return render(request, 'register.html', {'form': form})
-#
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(request.POST)
-#         if form.is_valid():
-#             email = form.cleaned_data['email']
-#             password = form.cleaned_data['password']
-#             user = authenticate(request, username=email, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 messages.success(request, "Logged in successfully!")
-#                 return redirect('base:home')
-#             else:
-#                 messages.error(request, "Invalid email or password")
-#     else:
-#         form = UserLoginForm()
-#
-#     return render(request, 'login.html', {'form': form})
-
-
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
@@ -100,9 +54,15 @@ def LoginUser(request):
             print("from login", user)
             print("current: ", timezone.now())
 
+            # Set session variable
+            request.session['last_login'] = str(timezone.now())
+
             messages.success(request, "Logged in successfully!")
             return redirect('base:home')
         else:
             messages.error(request, "Invalid username or password!")
             return redirect('users:login')
+
     return render(request, "login.html")
+
+
