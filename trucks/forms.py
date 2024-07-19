8593499598b72145a4e605146080cf56255d8fe3
Reithub <dhurshashi.v@gmail.com>
2024-07-19 03:54:12 -0400

    Modified urls and integrated truckform in common form

--- trucks/forms.py
+++ trucks/forms.py
@@ -4,14 +4,14 @@ from .models import Trucks
 class TruckForm(forms.ModelForm):
     class Meta:
         model = Trucks
-        fields = ['name', 'companyName', 'modelType', 'manufacturingYear', 'price', 'description', 'image', 'isNew']
+        fields = ['name', 'company_name', 'model_type', 'manufacturing_year', 'price', 'description', 'image', 'is_new']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'px-3 py-3 rounded w-full', 'placeholder': 'Bike name'}),
-            'companyName': forms.TextInput(attrs={'class': 'px-3 py-3 rounded w-full', 'placeholder': 'Company name'}),
-            'modelType': forms.Select(attrs={'class': 'px-3 py-3 rounded w-full'}),
-            'manufacturingYear': forms.NumberInput(attrs={'class': 'px-3 py-3 rounded w-full','placeholder': 'Manufacturing year'}),
+            'name': forms.TextInput(attrs={'class': 'px-3 py-3 rounded w-full', 'placeholder': 'Truck name'}),
+            'company_name': forms.TextInput(attrs={'class': 'px-3 py-3 rounded w-full', 'placeholder': 'Company name'}),
+            'model_type': forms.Select(attrs={'class': 'px-3 py-3 rounded w-full'}),
+            'manufacturing_year': forms.NumberInput(attrs={'class': 'px-3 py-3 rounded w-full','placeholder': 'Manufacturing year'}),
             'price': forms.NumberInput(attrs={'class': 'px-3 py-3 rounded w-full','placeholder': 'Price'}),
             'description': forms.Textarea(attrs={'class': 'px-3 py-3 rounded w-full', 'rows': 8,'placeholder': 'Description'}),
             'image': forms.URLInput(attrs={'class': 'px-3 py-3 rounded w-full', 'placeholder': 'Image URL'}),
-            'isNew': forms.CheckboxInput(attrs={'class': 'scale-125'}),
+            'is_new': forms.CheckboxInput(attrs={'class': 'scale-125'}),
         }
