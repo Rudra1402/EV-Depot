0856efe104b46e65e2d38e7d305197841cc768b6
Rudra Patel <rudra.190410116095@gmail.com>
2024-07-20 00:38:54 -0400

    secret key and minor changes

--- evdepot/settings.py
+++ evdepot/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-k!q34#4r495=do3-er(fhp-!9den898if*^pz_*n^8kcf4(+ah'
+SECRET_KEY = 'django-insecure-k!q34#4r495=do3-er(fhp-!9den898if*^pz_*n^8kcf4(+ah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
