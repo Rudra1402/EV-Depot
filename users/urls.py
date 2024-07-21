b08bbfc861b3bc85595775a3e34164ce7e61c487
chintan0513 <chintantripathi1310@gmail.com>
2024-07-21 02:56:21 -0400

    logout functionality created

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name= "users"
 urlpatterns = [
     path('register/', views.Register, name='register'),
     path('login/', views.LoginUser, name='login'),
+    path('logout/', views.LogoutUser, name='logout'),
 ]
