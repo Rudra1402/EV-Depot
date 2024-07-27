83414dae68c0f52f288a36b4acd8d93626707e9c
Reithub <dhurshashi.v@gmail.com>
2024-07-26 20:01:32 -0400

    Updated ratings for bikes page

--- bikes/admin.py
+++ bikes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bikes
+from .models import Bikes, Rating
 
 # Register your models here.
 admin.site.register(Bikes)
+admin.site.register(Rating)
